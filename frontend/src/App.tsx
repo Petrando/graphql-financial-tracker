0932c2fe834ab4cf76dbc184d907563fca419e21
Petrando <petrandorichard@gmail.com>
2024-08-10 20:45:12 +0800

    frontend and backend connected and ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
+import { Toaster } from "react-hot-toast";
+import { HomePage, LoginPage, NotFound as NotFoundPage, SignUpPage, TransactionPage } from './pages'
+import Header from './components/ui/Header';
 import './App.css'
 
 function App() {
+  const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+
+	if (loading) return null;
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+			{data?.authUser && <Header />}
+			<Routes>
+				<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route
+					path='/transaction/:id'
+					element={data.authUser ? <TransactionPage /> : <Navigate to='/login' />}
+				/>
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+			<Toaster />
+		</>
   )
 }
 
