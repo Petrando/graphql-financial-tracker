8d5b4e7b5048b1a4b1116723b78297858fbe8aa8
Petrando <petrandorichard@gmail.com>
2024-08-10 20:29:02 +0800

    wrap components in main.tsx with BrowserRouter and GridBackground and also setup Apollo client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import GridBackground from './components/ui/GridBackground.tsx'
+
+const client = new ApolloClient({
+	// TODO => Update the uri on production
+	uri: "http://localhost:4000/graphql",//import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // the URL of our GraphQL server.
+	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.	
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter >
+      <GridBackground>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </GridBackground>
+    </BrowserRouter>
   </StrictMode>,
 )
